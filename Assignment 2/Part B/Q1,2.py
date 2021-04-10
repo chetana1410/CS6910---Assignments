96d385b30d42285cab0d6b26125df177b8415dbd
chetana1410 <64744226+chetana1410@users.noreply.github.com>
2021-04-10 18:19:58 +0530

    Update Q1,2.py

--- Assignment 2/Part B/Q1,2.py
+++ Assignment 2/Part B/Q1,2.py
@@ -200,3 +200,39 @@ def train():
   callbacks=[WandbCallback(data_type='image',validation_data = val_it, verbose=1), EarlyStopping(patience=10, restore_best_weights=True)])
 
 
+  
+sweep_config = {
+   #'program': train(),
+    'method': 'bayes',         
+    'metric': {
+        'name': 'val_accuracy',     
+        'goal': 'maximize'      
+    },
+    'parameters': {
+        'learning_rate': {
+            'values': [0, 1e-3, 5*1e-4, 1e-4]   # 0 implies default learning rate set by model
+        },
+        'freeze': {
+            'values': [-1, 5, 10]
+        },
+        'epochs': {
+            'min': 5,
+            'max': 20
+        },
+        'data_aug': {
+            'values': [0,1]
+        },
+        'optimizer' : {
+            'values': ['Adam', 'rmsprop', 'SGD']
+        },
+        'tr_model' : {
+            'values': ['VGG16', 'InceptionV3', 'ResNet50', 'Xception', 'InceptionResNetV2']
+        },
+        'batch_size': {
+            'values': [32, 64]
+        }
+        }
+    }
+
+sweep_id = wandb.sweep(sweep_config, project='skQ2')
+wandb.agent(sweep_id, function=train)
