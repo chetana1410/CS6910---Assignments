f55e88a533a2c407d79fa5d62277252cb6756419
chetana1410 <64744226+chetana1410@users.noreply.github.com>
2021-05-26 02:48:59 +0530

    Update Q5b,Q5d,Q6.py

--- Assignment 3/Q5b,Q5d,Q6.py
+++ Assignment 3/Q5b,Q5d,Q6.py
@@ -350,42 +350,33 @@ print(score)
 
 
 # Q5d
-def plot_attention(ax,attention, sentence, predicted_sentence):
 
-   h= Path('/content/Lohit-Devanagari.ttf')
-
-   hindi_font = FontProperties(fname=h)
-   fig = plt.figure(figsize=(10, 10))
-   ax = fig.add_subplot(1, 1, 1)
-   ax.matshow(attention, cmap='viridis')
-
-   ax.set_xticklabels([''] + sentence, fontsize=14, rotation=90)
-   ax.set_yticklabels([''] + predicted_sentence, fontsize=25,fontproperties=hindi_font)
-
-   ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
-   ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
-
-   plt.show()
-
-def generate_indices1():
-  return  np.random.randint(low=0, high = test.shape[0], size=10)
-
-rnd = generate_indices1()
-
-def translate(ax,sentence):
- result, sentence, attention_plot = evaluate(sentence)
-
-  print('Input:', sentence)
-  print('Predicted translation:', result)
-
-  attention_plot = attention_plot[:len(result.split(' ')),
-                                    :len(sentence.split(' '))]
-  plot_attention(ax,attention_plot, sentence.split(' '), result.split(' '))
-
+from pathlib import Path
+from matplotlib.font_manager import FontProperties
+h= Path('/content/Lohit-Devanagari.ttf')
+hindi_font = FontProperties(fname=h)
+rnd = np.random.randint(low=0, high = test.shape[0], size=12)
+fig , ax = plt.subplots(4,3,figsize=(30,30))
 j=0
-for i in range(10):
-  translate(ax[j%4],test['word'].iloc[i])
+for i in rnd:
+  
+  sentence=test['english'].iloc[i]
+  result, sentence, attention_plot = evaluate(sentence)
+  attention_plot = attention_plot[:len(result.split(' ')),:len(sentence.split(' '))]
+  sentence = sentence.split(' ') 
+  predicted_sentence=result.split(' ')
+  ax[j//3][j%3].matshow(attention_plot, cmap='viridis')
+
+  ax[j//3][j%3].set_xticklabels([''] + sentence, fontsize=20, rotation=90)
+  ax[j//3][j%3].set_yticklabels([''] + predicted_sentence, fontsize=35,fontproperties=hindi_font)
+
+  ax[j//3][j%3].xaxis.set_major_locator(ticker.MultipleLocator(1))
+  ax[j//3][j%3].yaxis.set_major_locator(ticker.MultipleLocator(1))
+  ax[j//3][j%3].set_title('ATTENTION PLOT '+str(j+1),fontsize=20,fontweight='bold', pad=50)
   j+=1
+
+fig.tight_layout(pad=10)
+plt.show()
 
 
 
